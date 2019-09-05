60e23e3da1924e7bc6406fca215b35a3cca6ab5e
silvia <silviademiguel7@gmail.com>
2019-09-05 11:27:40 +0200

    create App two

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-import Landing from './Landing';
 
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 
